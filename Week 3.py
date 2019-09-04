e2dd3d842d346274b4dc9db687c10abd8649bf41
rudycastellon1 <54876907+rudycastellon1@users.noreply.github.com>
2019-09-04 01:02:51 -0700

    Add files via upload
    Asked to execute what type of variables were present.

---
+++ Week 3.py
@@ -0,0 +1,12 @@
+Python 3.7.3 (default, Apr 24 2019, 15:29:51) [MSC v.1915 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=3.5
+>>> print(type(x))
+<class 'float'>
+>>> y='3.5'
+>>> print(type(y))
+<class 'str'>
+>>> z={1:'John',2:'Wick',3:'Barry',4:'Allen'}
+>>> print(type(z))
+<class 'dict'>
+>>> 
