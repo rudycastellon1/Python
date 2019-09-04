7f8ebb84abc31761e3659cd311be835cdfc777ed
rudycastellon1 <54876907+rudycastellon1@users.noreply.github.com>
2019-09-04 01:29:19 -0700

    Add files via upload
    Given a word, write one line of command that print “True” if the first letter is the same as the last letter, and the length of the word is greater than 2. Otherwise, the program prints a “False”.

---
+++ Week 3 Problem 3.py
@@ -0,0 +1,9 @@
+Python 3.7.3 (default, Apr 24 2019, 15:29:51) [MSC v.1915 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x='mom'
+>>> x[0]==x[-1]
+True
+>>> x='no'
+>>> x[0]==x[-1]
+False
+>>> 
